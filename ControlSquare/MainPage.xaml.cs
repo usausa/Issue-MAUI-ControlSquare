fcc0c1e1be51398044041f419537a2efa14f8dbc
usausa <machi.pon@gmail.com>
2023-07-29 22:08:04 +0900

    Add project

---
+++ ControlSquare/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace ControlSquare;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"Height={BoxView.Height}, HeightRequest={BoxView.HeightRequest}");
+    }
+}
+
